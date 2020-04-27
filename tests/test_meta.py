4da693501a201d33a88c59859ae2e3e74d5cd2ab
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-27 13:46:45 +0200

    Add test to check sequence of translated proteins in `tests.test_meta`

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -13,12 +13,19 @@ class TestPyrodigalMeta(unittest.TestCase):
     def setUpClass(cls):
         data = os.path.realpath(os.path.join(__file__, "..", "data"))
         fna = os.path.join(data, "SRR492066.fna.gz")
-        faa = os.path.join(data, "SRR492066.meta.faa.gz")
+        meta_fna = os.path.join(data, "SRR492066.meta.fna.gz")
+        meta_faa = os.path.join(data, "SRR492066.meta.faa.gz")
 
         with gzip.open(fna, "rt") as f:
             cls.record = next(Bio.SeqIO.parse(f, "fasta"))
-        with gzip.open(faa, "rt") as f:
+        with gzip.open(meta_faa, "rt") as f:
             cls.proteins = [
+                record
+                for record in Bio.SeqIO.parse(f, "fasta")
+                if record.id.startswith("{}_".format(cls.record.id))
+            ]
+        with gzip.open(meta_fna, "rt") as f:
+            cls.genes = [
                 record
                 for record in Bio.SeqIO.parse(f, "fasta")
                 if record.id.startswith("{}_".format(cls.record.id))
@@ -27,5 +34,7 @@ class TestPyrodigalMeta(unittest.TestCase):
     def test_translate(self):
         p = Pyrodigal(meta=True)
         genes = p.find_genes(str(self.record.seq))
-
         self.assertEqual(len(genes), len(self.proteins))
+
+        for gene, protein in zip(genes, self.proteins):
+            self.assertEqual(gene.translate(), str(protein.seq))
