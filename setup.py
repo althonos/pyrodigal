29a245f38c3824f3ecb46121b403d504e198b3b3
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-09-04 15:21:05 +0200

    Update `setup.py` script to split the `training.c` file in chunks

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import configparser
+import glob
+import os
 import sys
 
 import setuptools
@@ -27,7 +29,32 @@ class build_ext(_build_ext):
     """A `build_ext` that disables optimizations if compiled in debug mode.
     """
 
+    def _write_source_split(self, training_temp, index, lines):
+        filename = os.path.join(training_temp, "training{:02}.c".format(index))
+        with open(filename, "wb") as dst:
+            if index != 0:
+                dst.write(b'#include "training.h"\n')
+            dst.writelines(lines)
+        lines.clear()
+
+    def split_training_source(self, training_file, training_temp):
+        self.mkpath(training_temp)
+        with open(training_file, "rb") as src:
+            lines = []
+            index = 0
+            for line in src:
+                if line.startswith(b"void initialize_metagenome"):
+                    self._write_source_split(training_temp, index, lines)
+                    index += 1
+                lines.append(line)
+            self._write_source_split(training_temp, index, lines)
+
     def build_extension(self, ext):
+        training_file = ext.sources.pop()
+        training_temp = os.path.join(self.build_temp, "training")
+        self.make_file([training_file], training_temp, self.split_training_source, (training_file, training_temp))
+        ext.sources.extend(sorted(glob.glob(os.path.join(training_temp, "*.c"))))
+
         if self.debug:
             if sys.platform == "linux" or sys.platform == "darwin":
                 ext.extra_compile_args.append("-O0")
@@ -49,6 +76,7 @@ extensions = [
             "Prodigal/metagenomic.c",
             "Prodigal/node.c",
             "Prodigal/sequence.c",
+            # leave this last
             "Prodigal/training.c"
         ],
         include_dirs=["Prodigal"],
