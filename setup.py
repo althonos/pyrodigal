c4ba4f25e0f4650e60152b3f2e0328dd862a485a
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-27 18:44:22 +0100

    Rewrite `setup.py` to build a static library with Prodigal code

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ import sys
 import setuptools
 from Cython.Build import cythonize
 from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.build_clib import build_clib as _build_clib
 from setuptools.command.sdist import sdist as _sdist
+from setuptools.extension import Extension, Library
 
 
 class sdist(_sdist):
@@ -28,6 +30,35 @@ class sdist(_sdist):
 class build_ext(_build_ext):
     """A `build_ext` that disables optimizations if compiled in debug mode.
     """
+
+    def build_extension(self, ext):
+        if self.debug:
+            if sys.platform == "linux" or sys.platform == "darwin":
+                ext.extra_compile_args.append("-O0")
+            if sys.implementation.name == "cpython":
+                ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
+        _build_ext.build_extension(self, ext)
+
+    def run(self):
+        self.run_command("build_clib")
+        _build_ext.run(self)
+
+class build_clib(_build_clib):
+    """A custom `build_clib` that compiles out of source.
+    """
+
+    # --- Compatibility with base `build_clib` command ---
+
+    def check_library_list(self, libraries):
+        pass
+
+    def get_library_names(self):
+        return [ lib.name for lib in self.libraries ]
+
+    def get_source_files(self):
+        return [ source for lib in self.libraries for source in lib.sources ]
+
+    # --- Build code ---
 
     def _write_source_split(self, training_temp, index, lines):
         filename = os.path.join(training_temp, "training{:02}.c".format(index))
@@ -51,43 +82,68 @@ class build_ext(_build_ext):
                 lines.append(line)
             self._write_source_split(training_temp, index, lines)
 
-    def build_extension(self, ext):
-        training_file = ext.sources.pop()
+    def run(self):
+        # split the huge `training.c` file in small chunks with individual
+        # functions so that it can compile even on low-memory machines
+        lib = self.libraries[0]
+        training_file = next(s for s in lib.sources if os.path.basename(s) == "training.c")
         training_temp = os.path.join(self.build_temp, "training")
         self.make_file([training_file], training_temp, self.split_training_source, (training_file, training_temp))
-        ext.sources.extend(sorted(glob.glob(os.path.join(training_temp, "*.c"))))
 
-        if self.debug:
-            if sys.platform == "linux" or sys.platform == "darwin":
-                ext.extra_compile_args.append("-O0")
-            if sys.implementation.name == "cpython":
-                ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
-        _build_ext.build_extension(self, ext)
+        # patch the library sources to use the split training files
+        lib.sources.remove(training_file)
+        lib.sources.extend(sorted(glob.glob(os.path.join(training_temp, "*.c"))))
 
+        # build the library as normal
+        _build_clib.run(self)
+
+    def build_libraries(self, libraries):
+        self.mkpath(self.build_clib)
+        for library in libraries:
+            self.make_file(
+                library.sources,
+                self.compiler.library_filename(library.name, output_dir=self.build_clib),
+                self.build_library,
+                (library,)
+            )
+
+    def build_library(self, library):
+        objects = self.compiler.compile(
+            library.sources,
+            output_dir=self.build_temp,
+            include_dirs=library.include_dirs + [self.build_clib],
+            macros=library.define_macros,
+            debug=self.debug,
+            depends=library.depends,
+            extra_preargs=library.extra_compile_args,
+        )
+        self.compiler.create_static_lib(
+            objects,
+            library.name,
+            output_dir=self.build_clib,
+            debug=self.debug,
+        )
+
+libraries = [
+    Library(
+        "prodigal",
+        sources=glob.glob(os.path.join("Prodigal", "*.c")),
+        include_dirs=["Prodigal"],
+    )
+]
 
 extensions = [
-    setuptools.Extension(
+    Extension(
         "pyrodigal._pyrodigal",
-        [
-            "pyrodigal/__init__.pyx",
-            "Prodigal/bitmap.c",
-            "Prodigal/dprog.c",
-            "Prodigal/gene.c",
-            "Prodigal/metagenomic.c",
-            "Prodigal/node.c",
-            "Prodigal/sequence.c",
-            # leave this last
-            "Prodigal/training.c"
-        ],
+        sources=["pyrodigal/__init__.pyx"],
         include_dirs=["Prodigal"],
-        libraries=[],
-        library_dirs=[],
-        extra_compile_args=["-Wall"],
+        libraries=["prodigal"],
     ),
 ]
 
 
 setuptools.setup(
+    libraries=libraries,
     ext_modules=cythonize(extensions, annotate=True),
-    cmdclass=dict(sdist=sdist, build_ext=build_ext),
+    cmdclass=dict(sdist=sdist, build_ext=build_ext, build_clib=build_clib),
 )
