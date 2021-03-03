2735824cddd98c53f54b2a295a82e3bf01c4be60
Martin Larralde <martin.larralde@embl.de>
2021-03-03 17:29:26 +0100

    Install twine in AppVeyor to fix broken builds

--- ci/appveyor/deploy.ps1
+++ ci/appveyor/deploy.ps1
@@ -1,5 +1,4 @@
 If ($env:APPVEYOR_REPO_TAG -eq "true") {
-    Invoke-Expression "$env:PYTHON\\python.exe -m pip install twine"
     Invoke-Expression "$env:PYTHON\\python.exe -m twine upload --skip-existing dist\\*.whl"
 } Else {
     write-output "Not on a tag on master, won't deploy to PyPI"
