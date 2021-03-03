414ff41036c901261a2e49d51f4e68b2d637d8af
Martin Larralde <martin.larralde@embl.de>
2021-03-03 15:41:05 +0100

    Remove `twine` from CI dependencies and only install it when needed

--- ci/appveyor/deploy.ps1
+++ ci/appveyor/deploy.ps1
@@ -1,4 +1,5 @@
 If ($env:APPVEYOR_REPO_TAG -eq "true") {
+    Invoke-Expression "$env:PYTHON\\python.exe -m pip install twine"
     Invoke-Expression "$env:PYTHON\\python.exe -m twine upload --skip-existing dist\\*.whl"
 } Else {
     write-output "Not on a tag on master, won't deploy to PyPI"
