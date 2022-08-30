762b14b52c430fef268de84711befeacd9f41a6f
Martin Larralde <martin.larralde@embl.de>
2022-08-30 18:22:59 +0200

    Use the `vbic` instruction in NEON instead of combining bitwise AND and NOT instructions

--- pyrodigal/impl/neon.c
+++ pyrodigal/impl/neon.c
@@ -39,45 +39,45 @@ void skippable_neon(
       s          = vdupq_n_u8(0);
       // 5'fwd->5'fwd
       // n1->strand == n2->strand && n2->type != STOP && n1->type != STOP
-      x =                  vceqq_u8(n1_strands, n2_strands);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n2_types, all_stops)),    x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_types, all_stops)),    x);
-      s = vorrq_u8(                                         s, x);
+      x =             vceqq_u8(n1_strands, n2_strands);
+      x = vbicq_u8(x, vceqq_u8(n2_types, all_stops));
+      x = vbicq_u8(x, vceqq_u8(n1_types, all_stops));
+      s = vorrq_u8(x, s);
       // 5'fwd->5'ref, 5'fwd->3'rev
       // n2->strand == -1 && n1->strand == 1 && n1->type != STOP
-      x =                  vceqq_u8(n2_strands, all_bwd);
-      x = vandq_u8(   vceqq_u8(n1_strands, all_fwd), x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_types, all_stops)), x);
-      s = vorrq_u8(                                      s, x);
+      x =             vceqq_u8(n2_strands, all_bwd);
+      x = vandq_u8(x, vceqq_u8(n1_strands, all_fwd));
+      x = vbicq_u8(x, vceqq_u8(n1_types, all_stops));
+      s = vorrq_u8(x, s);
       // 5'fwd
       // n1->type == STOP && n1->strand == -1 && n2->strand == -1
-      x =               vceqq_u8(n1_types, all_stops);
-      x = vandq_u8(vceqq_u8(n1_strands, all_bwd), x);
-      x = vandq_u8(vceqq_u8(n2_strands, all_fwd), x);
-      s = vorrq_u8(                                   s, x);
+      x =             vceqq_u8(n1_types, all_stops);
+      x = vandq_u8(x, vceqq_u8(n1_strands, all_bwd));
+      x = vandq_u8(x, vceqq_u8(n2_strands, all_fwd));
+      s = vorrq_u8(x, s);
       // 5'rev->3'fwd
       // n2->type == STOP && n1->strand == -1 && n2->strand == 1 && n1->type != STOP
-      x =                  vceqq_u8(n2_types, all_stops);
-      x = vandq_u8(   vceqq_u8(n1_strands, all_bwd), x);
-      x = vandq_u8(   vceqq_u8(n2_strands, all_fwd), x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_types, all_stops)), x);
-      s = vorrq_u8(                                      s, x);
+      x =             vceqq_u8(n2_types, all_stops);
+      x = vandq_u8(x, vceqq_u8(n1_strands, all_bwd));
+      x = vandq_u8(x, vceqq_u8(n2_strands, all_fwd));
+      x = vbicq_u8(x, vceqq_u8(n1_types, all_stops));
+      s = vorrq_u8(x, s);
       // 5'fwd->3'fwd
       // n1->strand == n2->strand && n1->strand == 1 && n1->type != STOP && n2->type == STOP && n1->ndx%3 != n2->ndx%3
-      x =                  vceqq_u8(n1_strands, n2_strands);
-      x = vandq_u8(   vceqq_u8(n1_strands, all_fwd),    x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_types,   all_stops)),  x);
-      x = vandq_u8(   vceqq_u8(n2_types,   all_stops),  x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_frames,  n2_frames)),  x);
-      s = vorrq_u8(                                        s,  x);
+      x =             vceqq_u8(n1_strands, n2_strands);
+      x = vandq_u8(x, vceqq_u8(n1_strands, all_fwd));
+      x = vbicq_u8(x, vceqq_u8(n1_types,   all_stops));
+      x = vandq_u8(x, vceqq_u8(n2_types,   all_stops));
+      x = vbicq_u8(x, vceqq_u8(n1_frames,  n2_frames));
+      s = vorrq_u8(x, s);
       // 3'rev->5'rev
       // n1->strand == n2->strand && n1->strand == -1 && n1->type == STOP && n2->type != STOP && n1->ndx%3 != n2->ndx%3
-      x =                   vceqq_u8(n1_strands, n2_strands);
-      x = vandq_u8(         vceqq_u8(n1_strands, all_bwd),    x);
-      x = vandq_u8(         vceqq_u8(n1_types,   all_stops),  x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n2_types,   all_stops)), x);
-      x = vandq_u8(vmvnq_u8(vceqq_u8(n1_frames,  n2_frames)), x);
-      s = vorrq_u8(                                        s, x);
+      x =             vceqq_u8(n1_strands, n2_strands);
+      x = vandq_u8(x, vceqq_u8(n1_strands, all_bwd));
+      x = vandq_u8(x, vceqq_u8(n1_types,   all_stops));
+      x = vbicq_u8(x, vceqq_u8(n2_types,   all_stops));
+      x = vbicq_u8(x, vceqq_u8(n1_frames,  n2_frames));
+      s = vorrq_u8(x, s);
 
       // store result mask
       vst1q_u8(&skip[j], s);
