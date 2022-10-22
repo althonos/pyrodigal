d69cc17d21b8c7d9fd91276d579826d7718ff79e
Martin Larralde <martin.larralde@embl.de>
2022-10-22 15:55:01 +0200

    Refactor connection scorer code to test all supported SIMD backends

--- pyrodigal/tests/test_connection_scorer.py
+++ pyrodigal/tests/test_connection_scorer.py
@@ -11,6 +11,8 @@ from .fasta import parse
 
 
 class TestConnectionScorer(unittest.TestCase):
+    backend = "generic"
+
     def assertNodeEqual(self, n1, n2):
         self.assertEqual(n1.index, n2.index)
         self.assertEqual(n1.strand, n2.strand)
@@ -31,177 +33,55 @@ class TestConnectionScorer(unittest.TestCase):
         with gzip.open(fna, "rt") as f:
             cls.record = next(parse(f))
 
-    @unittest.skipUnless(_pyrodigal._TARGET_CPU == "x86", "requires x86 CPU")
-    @unittest.skipUnless(
-        _pyrodigal._MMX_BUILD_SUPPORT, "requires extension compiled with MMX support"
-    )
-    @unittest.skipUnless(
-        _pyrodigal._MMX_RUNTIME_SUPPORT, "requires machine with MMX support"
-    )
-    def test_score_connections_mmx(self):
+    def test_score_connections(self):
         # setup
         seq = Sequence.from_string(self.record.seq)
         tinf = METAGENOMIC_BINS[0].training_info
-        scorer_mmx = ConnectionScorer(backend="mmx")
+        scorer_simd = ConnectionScorer(backend=self.backend)
         scorer_none = ConnectionScorer(backend=None)
         # add nodes from the sequence
         nodes = Nodes()
         nodes.extract(seq, translation_table=tinf.translation_table)
         nodes.sort()
         # index nodes for the scorers
-        scorer_mmx.index(nodes)
+        scorer_simd.index(nodes)
         scorer_none.index(nodes)
         # use copies to compute both scores
-        nodes_mmx = nodes.copy()
+        nodes_simd = nodes.copy()
         nodes_none = nodes.copy()
         for i in range(500, len(nodes)):
-            # compute boundary
+            # compute boundary (MAX_NODE_DIST = 500)
             j = 0 if i < 500 else i - 500
             # score connections without fast-indexing skippable nodes
             scorer_none.compute_skippable(j, i)
             scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
             # compute skippable nodes with MMX and score connections with
-            scorer_mmx.compute_skippable(j, i)
-            scorer_mmx.score_connections(nodes_mmx, j, i, tinf, final=True)
+            scorer_simd.compute_skippable(j, i)
+            scorer_simd.score_connections(nodes_simd, j, i, tinf, final=True)
         # check that both methods scored the same
-        for n1, n2 in zip(nodes_mmx, nodes_none):
+        for n1, n2 in zip(nodes_simd, nodes_simd):
             self.assertNodeEqual(n1, n2)
 
-    @unittest.skipUnless(_pyrodigal._TARGET_CPU == "x86", "requires x86 CPU")
-    @unittest.skipUnless(
-        _pyrodigal._SSE2_BUILD_SUPPORT, "requires extension compiled with SSE2 support"
-    )
-    @unittest.skipUnless(
-        _pyrodigal._SSE2_RUNTIME_SUPPORT, "requires machine with SSE2 support"
-    )
-    def test_score_connections_sse(self):
-        # setup
-        seq = Sequence.from_string(self.record.seq)
-        tinf = METAGENOMIC_BINS[0].training_info
-        scorer_sse = ConnectionScorer(backend="sse")
-        scorer_none = ConnectionScorer(backend=None)
-        # add nodes from the sequence
-        nodes = Nodes()
-        nodes.extract(seq, translation_table=tinf.translation_table)
-        nodes.sort()
-        # index nodes for the scorers
-        scorer_sse.index(nodes)
-        scorer_none.index(nodes)
-        # use copies to compute both scores
-        nodes_sse = nodes.copy()
-        nodes_none = nodes.copy()
-        for i in range(500, len(nodes)):
-            # compute boundary
-            j = 0 if i < 500 else i - 500
-            # score connections without fast-indexing skippable nodes
-            scorer_none.compute_skippable(j, i)
-            scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
-            # compute skippable nodes with SSE and score connections with
-            scorer_sse.compute_skippable(j, i)
-            scorer_sse.score_connections(nodes_sse, j, i, tinf, final=True)
-        # check that both methods scored the same
-        for n1, n2 in zip(nodes_sse, nodes_none):
-            self.assertNodeEqual(n1, n2)
 
-    @unittest.skipUnless(_pyrodigal._TARGET_CPU == "x86", "requires x86 CPU")
-    @unittest.skipUnless(
-        _pyrodigal._AVX2_BUILD_SUPPORT, "requires extension compiled with AVX2 support"
-    )
-    @unittest.skipUnless(
-        _pyrodigal._AVX2_RUNTIME_SUPPORT, "requires machine with AVX2 support"
-    )
-    def test_score_connections_avx(self):
-        # setup
-        seq = Sequence.from_string(self.record.seq)
-        tinf = METAGENOMIC_BINS[0].training_info
-        scorer_avx = ConnectionScorer(backend="avx")
-        scorer_none = ConnectionScorer(backend=None)
-        # add nodes from the sequence
-        nodes = Nodes()
-        nodes.extract(seq, translation_table=tinf.translation_table)
-        nodes.sort()
-        # index nodes for the scorers
-        scorer_avx.index(nodes)
-        scorer_none.index(nodes)
-        # use copies to compute both scores
-        nodes_avx = nodes.copy()
-        nodes_none = nodes.copy()
-        for i in range(500, len(nodes)):
-            # compute boundary
-            j = 0 if i < 500 else i - 500
-            # score connections without fast-indexing skippable nodes
-            scorer_none.compute_skippable(j, i)
-            scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
-            # compute skippable nodes with SSE and score connections with
-            scorer_avx.compute_skippable(j, i)
-            scorer_avx.score_connections(nodes_avx, j, i, tinf, final=True)
-        # check that both methods scored the same
-        for n1, n2 in zip(nodes_avx, nodes_none):
-            self.assertNodeEqual(n1, n2)
+@unittest.skipUnless(_pyrodigal._MMX_BUILD_SUPPORT, "extension compiled without MMX support")
+@unittest.skipUnless(_pyrodigal._MMX_RUNTIME_SUPPORT, "requires machine with MMX support")
+class TestConnectionScorerMMX(TestConnectionScorer):
+    backend = "mmx"
 
-    @unittest.skipUnless(
-        _pyrodigal._TARGET_CPU in ("arm", "aarch64"), "requires ARM CPU"
-    )
-    @unittest.skipUnless(
-        _pyrodigal._NEON_BUILD_SUPPORT, "requires extension compiled with NEON support"
-    )
-    @unittest.skipUnless(
-        _pyrodigal._NEON_RUNTIME_SUPPORT, "requires machine with NEON support"
-    )
-    def test_score_connections_neon(self):
-        # setup
-        seq = Sequence.from_string(self.record.seq)
-        tinf = METAGENOMIC_BINS[0].training_info
-        scorer_avx = ConnectionScorer(backend="neon")
-        scorer_none = ConnectionScorer(backend=None)
-        # add nodes from the sequence
-        nodes = Nodes()
-        nodes.extract(seq, translation_table=tinf.translation_table)
-        nodes.sort()
-        # index nodes for the scorers
-        scorer_avx.index(nodes)
-        scorer_none.index(nodes)
-        # use copies to compute both scores
-        nodes_avx = nodes.copy()
-        nodes_none = nodes.copy()
-        for i in range(500, len(nodes)):
-            # compute boundary
-            j = 0 if i < 500 else i - 500
-            # score connections without fast-indexing skippable nodes
-            scorer_none.compute_skippable(j, i)
-            scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
-            # compute skippable nodes with SSE and score connections with
-            scorer_avx.compute_skippable(j, i)
-            scorer_avx.score_connections(nodes_avx, j, i, tinf, final=True)
-        # check that both methods scored the same
-        for n1, n2 in zip(nodes_avx, nodes_none):
-            self.assertNodeEqual(n1, n2)
 
-    def test_score_connections_generic(self):
-        # setup
-        seq = Sequence.from_string(self.record.seq)
-        tinf = METAGENOMIC_BINS[0].training_info
-        scorer_generic = ConnectionScorer(backend="generic")
-        scorer_none = ConnectionScorer(backend=None)
-        # add nodes from the sequence
-        nodes = Nodes()
-        nodes.extract(seq, translation_table=tinf.translation_table)
-        nodes.sort()
-        # index nodes for the scorers
-        scorer_generic.index(nodes)
-        scorer_none.index(nodes)
-        # use copies to compute both scores
-        nodes_generic = nodes.copy()
-        nodes_none = nodes.copy()
-        for i in range(500, len(nodes)):
-            # compute boundary
-            j = 0 if i < 500 else i - 500
-            # score connections without fast-indexing skippable nodes
-            scorer_none.compute_skippable(j, i)
-            scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
-            # compute skippable nodes with generic filter and score connections
-            scorer_generic.compute_skippable(j, i)
-            scorer_generic.score_connections(nodes_generic, j, i, tinf, final=True)
-        # check that both methods scored the same
-        for n1, n2 in zip(nodes_generic, nodes_none):
-            self.assertNodeEqual(n1, n2)
+@unittest.skipUnless(_pyrodigal._SSE2_BUILD_SUPPORT, "extension compiled without SSE2 support")
+@unittest.skipUnless(_pyrodigal._SSE2_RUNTIME_SUPPORT, "requires machine with SSE2 support")
+class TestConnectionScorerSSE(TestConnectionScorer):
+    backend = "sse"
+
+
+@unittest.skipUnless(_pyrodigal._AVX2_BUILD_SUPPORT, "extension compiled without AVX2 support")
+@unittest.skipUnless(_pyrodigal._AVX2_RUNTIME_SUPPORT, "requires machine with AVX2 support")
+class TestConnectionScorerAVX(TestConnectionScorer):
+    backend = "avx"
+
+
+@unittest.skipUnless(_pyrodigal._NEON_BUILD_SUPPORT, "extension compiled without NEON support")
+@unittest.skipUnless(_pyrodigal._NEON_RUNTIME_SUPPORT, "requires machine with NEON support")
+class TestConnectionScorerNEON(TestConnectionScorer):
+    backend = "neon"
