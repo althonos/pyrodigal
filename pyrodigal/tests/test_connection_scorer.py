fa5f01f5835b609f23d1ca2ab0d9393d98b1802e
Martin Larralde <martin.larralde@embl.de>
2022-10-24 22:47:33 +0200

    Add tests to check the connection scoring step while in training mode

--- pyrodigal/tests/test_connection_scorer.py
+++ pyrodigal/tests/test_connection_scorer.py
@@ -4,7 +4,7 @@ import os
 import sys
 import unittest
 
-from .. import Nodes, Sequence, _pyrodigal
+from .. import TrainingInfo, Nodes, Sequence, _pyrodigal
 from .._pyrodigal import METAGENOMIC_BINS, ConnectionScorer
 
 from .fasta import parse
@@ -32,8 +32,11 @@ class TestConnectionScorer(unittest.TestCase):
         fna = os.path.join(data, "MIIJ01000039.fna.gz")
         with gzip.open(fna, "rt") as f:
             cls.record = next(parse(f))
+        fna_train = os.path.join(data, "GCF_000009045.1_ASM904v1_genomic.fna.gz")
+        with gzip.open(fna_train, "rt") as f:
+            cls.record_train = next(parse(f))
 
-    def test_score_connections(self):
+    def test_score_connections_final(self):
         # setup
         seq = Sequence.from_string(self.record.seq)
         tinf = METAGENOMIC_BINS[0].training_info
@@ -55,11 +58,41 @@ class TestConnectionScorer(unittest.TestCase):
             # score connections without fast-indexing skippable nodes
             scorer_none.compute_skippable(j, i)
             scorer_none.score_connections(nodes_none, j, i, tinf, final=True)
-            # compute skippable nodes with MMX and score connections with
+            # compute skippable nodes with SIMD and then score connections
             scorer_simd.compute_skippable(j, i)
             scorer_simd.score_connections(nodes_simd, j, i, tinf, final=True)
         # check that both methods scored the same
-        for n1, n2 in zip(nodes_simd, nodes_simd):
+        for n1, n2 in zip(nodes_none, nodes_simd):
+            self.assertNodeEqual(n1, n2)
+
+    def test_score_connections_training(self):
+        # setup
+        seq = Sequence.from_string(self.record_train.seq)
+        tinf = TrainingInfo(seq.gc, translation_table=11)
+        scorer_simd = ConnectionScorer(backend=self.backend)
+        scorer_none = ConnectionScorer(backend=None)
+        # add nodes from the sequence
+        nodes = Nodes()
+        nodes.extract(seq, closed=True)
+        nodes.sort()
+        self.assertEqual(len(nodes), 196059)
+        # index nodes for the scorers
+        scorer_simd.index(nodes)
+        scorer_none.index(nodes)
+        # use copies to compute both scores
+        nodes_simd = nodes.copy()
+        nodes_none = nodes.copy()
+        for i in range(500, len(nodes)):
+            # compute boundary (MAX_NODE_DIST = 500)
+            j = 0 if i < 500 else i - 500
+            # score connections without fast-indexing skippable nodes
+            scorer_none.compute_skippable(j, i)
+            scorer_none.score_connections(nodes_none, j, i, tinf, final=False)
+            # compute skippable nodes with SIMD and then score connections
+            scorer_simd.compute_skippable(j, i)
+            scorer_simd.score_connections(nodes_simd, j, i, tinf, final=False)
+        # check that both methods scored the same
+        for n1, n2 in zip(nodes_none, nodes_simd):
             self.assertNodeEqual(n1, n2)
 
 
