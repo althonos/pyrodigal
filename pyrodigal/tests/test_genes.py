2fcb7a8044bb788f99278acc97df81eacbd7f710
Martin Larralde <martin.larralde@embl.de>
2023-09-27 12:15:26 +0200

    Add proper unit tests for `Genes.write_scores`

--- pyrodigal/tests/test_genes.py
+++ pyrodigal/tests/test_genes.py
@@ -54,36 +54,6 @@ class TestGenes(unittest.TestCase):
         if sys.version_info >= (3, 6):
             self.assertIsInstance(self.genes, collections.abc.Reversible)
 
-    def test_write_scores(self):
-        buffer = io.StringIO()
-        self.genes.write_scores(buffer, self.record.id)
-        actual = [
-            line.strip()
-            for line in buffer.getvalue().splitlines()
-            if not line.startswith("#") and line.strip()
-        ]
-        expected = [
-            line.strip()
-            for line in data.load_text("SRR492066.meta.tsv").splitlines()
-            if not line.startswith("#") and line.strip()
-        ]
-
-        r1 = csv.reader(actual, dialect="excel-tab")
-        r2 = csv.reader(expected, dialect="excel-tab")
-        for row1, row2 in zip(r1, r2):
-            self.assertEqual(row1[0], row2[0], "begin coordinates differ")
-            self.assertEqual(row1[1], row2[1], "end coordinates differ")
-            self.assertEqual(row1[2], row2[2], "strands differ")
-            self.assertEqual(row1[6], row2[6], "codons differ")
-            self.assertEqual(row1[7], row2[7], "RBS motifs differ")
-            self.assertEqual(row1[8], row2[8], "RBS spacers differ")
-
-        # NOTE(@althonos): This is rather convoluted but unfortunately we cannot
-        #                  compare lines directly because of rounding issues that
-        #                  can occur between different platforms: for instance,
-        #                  depending on the compilation flags, the score
-        #                  `-32.2550` may be rounded as `-32.25` or `-32.26`.
-
     def test_pickle(self):
         genes = pickle.loads(pickle.dumps(self.genes))
         mb = self.genes.metagenomic_bin
@@ -105,7 +75,7 @@ class _TestWrite(object):
         cls.record = data.load_record("SRR492066.fna.gz")
         cls.p = GeneFinder(meta=True)
         cls.genes = cls.p.find_genes(str(cls.record.seq))
-    
+
     def _write(self, genes, buffer, **kwargs):
         raise NotImplementedError
 
@@ -121,7 +91,7 @@ class _TestWrite(object):
 
 
 class TestWriteGFF(_TestWrite, unittest.TestCase):
-    
+
     def _write(self, genes, buffer, **kwargs):
         return genes.write_gff(buffer, self.record.id, **kwargs)
 
@@ -192,3 +162,39 @@ class TestWriteGenbank(_TestWrite, unittest.TestCase):
 
     def _write(self, genes, buffer, **kwargs):
         return genes.write_genbank(buffer, self.record.id, **kwargs)
+
+
+class TestWriteScores(_TestWrite, unittest.TestCase):
+
+    def _write(self, genes, buffer, **kwargs):
+        return genes.write_scores(buffer, self.record.id, **kwargs)
+
+    def test_example(self):
+        buffer = io.StringIO()
+        self._write(self.genes, buffer)
+        actual = [
+            line.strip()
+            for line in buffer.getvalue().splitlines()
+            if not line.startswith("#") and line.strip()
+        ]
+        expected = [
+            line.strip()
+            for line in data.load_text("SRR492066.meta.tsv").splitlines()
+            if not line.startswith("#") and line.strip()
+        ]
+
+        r1 = csv.reader(actual, dialect="excel-tab")
+        r2 = csv.reader(expected, dialect="excel-tab")
+        for row1, row2 in zip(r1, r2):
+            self.assertEqual(row1[0], row2[0], "begin coordinates differ")
+            self.assertEqual(row1[1], row2[1], "end coordinates differ")
+            self.assertEqual(row1[2], row2[2], "strands differ")
+            self.assertEqual(row1[6], row2[6], "codons differ")
+            self.assertEqual(row1[7], row2[7], "RBS motifs differ")
+            self.assertEqual(row1[8], row2[8], "RBS spacers differ")
+
+        # NOTE(@althonos): This is rather convoluted but unfortunately we cannot
+        #                  compare lines directly because of rounding issues that
+        #                  can occur between different platforms: for instance,
+        #                  depending on the compilation flags, the score
+        #                  `-32.2550` may be rounded as `-32.25` or `-32.26`.
