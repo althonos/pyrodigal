1670d76455aac2daace52544000ea608749d3452
Martin Larralde <martin.larralde@embl.de>
2023-11-06 01:28:25 +0100

    Remove `test_orf_finder` module to `test_gene_finder`

--- pyrodigal/tests/__init__.py
+++ pyrodigal/tests/__init__.py
@@ -2,9 +2,9 @@ from . import (
     test_connection_scorer,
     test_gene,
     test_genes,
+    test_gene_finder,
     test_mask,
     test_nodes,
-    test_orf_finder,
     test_sequence,
     test_training_info,
     test_doctest,
@@ -15,9 +15,9 @@ def load_tests(loader, suite, pattern):
     suite.addTests(loader.loadTestsFromModule(test_connection_scorer))
     suite.addTests(loader.loadTestsFromModule(test_gene))
     suite.addTests(loader.loadTestsFromModule(test_genes))
+    suite.addTests(loader.loadTestsFromModule(test_gene_finder))
     suite.addTests(loader.loadTestsFromModule(test_mask))
     suite.addTests(loader.loadTestsFromModule(test_nodes))
-    suite.addTests(loader.loadTestsFromModule(test_orf_finder))
     suite.addTests(loader.loadTestsFromModule(test_sequence))
     suite.addTests(loader.loadTestsFromModule(test_training_info))
     test_doctest.load_tests(loader, suite, pattern)
