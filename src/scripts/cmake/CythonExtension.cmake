91b9d04fc1c33e6bcfb7b7420ba12895839dfafb
Martin Larralde <martin.larralde@embl.de>
2024-09-04 01:10:25 +0200

    Add configuration files to build project with CMake

---
+++ src/scripts/cmake/CythonExtension.cmake
@@ -0,0 +1,82 @@
+find_package(Python COMPONENTS Interpreter Development.Module REQUIRED)
+get_property(PYTHON_EXTENSIONS_SOURCE_DIR GLOBAL PROPERTY PYTHON_EXTENSIONS_SOURCE_DIR)
+
+set(CYTHON_DIRECTIVES
+    -X cdivision=True
+    -X nonecheck=False
+    -E SSE2_BUILD_SUPPORT=$<IF:$<BOOL:${HAVE_SSE2}>,True,False>
+    -E AVX2_BUILD_SUPPORT=$<IF:$<BOOL:${HAVE_AVX2}>,True,False>
+    -E NEON_BUILD_SUPPORT=$<IF:$<BOOL:${HAVE_NEON}>,True,False>
+    -E MMX_BUILD_SUPPORT=False
+    -E AVX512_BUILD_SUPPORT=False
+    -E SYS_IMPLEMENTATION_NAME="cpython"
+    -E SYS_VERSION_INFO_MAJOR=${Python_VERSION_MAJOR}
+    -E SYS_VERSION_INFO_MINOR=${Python_VERSION_MINOR}
+    -E TARGET_CPU=${CMAKE_SYSTEM_PROCESSOR}
+    -E TARGET_SYSTEM="linux"
+    -E PYPY=$<IF:$<STREQUAL:${Python_INTERPRETER_ID},PyPy>,True,False>
+)
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug)
+  set(CYTHON_DIRECTIVES
+    ${CYTHON_DIRECTIVES}
+    -X cdivision_warnings=True
+    -X warn.undeclared=True
+    -X warn.unreachable=True
+    -X warn.maybe_uninitialized=True
+    -X warn.unused=True
+    -X warn.unused_arg=True
+    -X warn.unused_result=True
+    -X warn.multiple_declarators=True
+  )
+else()
+  set(CYTHON_DIRECTIVES
+    ${CYTHON_DIRECTIVES}
+    -X boundscheck=True
+    -X wraparound=True
+  )
+endif()
+
+macro(cython_extension _name)
+  cmake_parse_arguments(CYTHON_EXTENSION "" "" "LINKS" ${ARGN} )
+
+  # Make sure that the source directory is known
+  if(NOT DEFINED PYTHON_EXTENSIONS_SOURCE_DIR)
+    message(FATAL_ERROR "The PYTHON_EXTENSIONS_SOURCE_DIR variable has not been set.")
+  endif()
+
+  # Generate C file from Cython file
+  add_custom_command(
+    OUTPUT ${_name}.c
+    COMMENT
+      "Cythonizing ${_name}.pyx to ${_name}.c"
+    COMMAND
+      Python::Interpreter -m cython
+        "${CMAKE_CURRENT_SOURCE_DIR}/${_name}.pyx"
+        --output-file ${_name}.c
+        -I "${CYTHON_HEADERS_DIR}"
+        ${CYTHON_DIRECTIVES}
+    MAIN_DEPENDENCY
+      ${_name}.pyx
+    VERBATIM)
+
+  # Build fully-qualified module name as the target name
+  string(REGEX REPLACE "^${PYTHON_EXTENSIONS_SOURCE_DIR}/?" "" _dest_folder ${CMAKE_CURRENT_SOURCE_DIR})
+  string(REPLACE "/" "." _target ${_dest_folder}.${_name})
+
+  # Add Python module
+  python_add_library(${_target} MODULE WITH_SOABI ${_name}.pyx ${_name}.pxd ${_name}.c)
+  set_target_properties(${_target} PROPERTIES OUTPUT_NAME ${_name} )
+  target_include_directories(${_target} AFTER PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})  
+  target_link_libraries(${_target} PUBLIC ${CYTHON_EXTENSION_LINKS})
+
+  # Preserve the relative project structure in the install directory
+  string(REGEX REPLACE "^${PYTHON_EXTENSIONS_SOURCE_DIR}/?" "" _dest_folder ${CMAKE_CURRENT_SOURCE_DIR})
+  install(TARGETS ${_target} DESTINATION ${_dest_folder} )
+  message(DEBUG "Install folder for extension ${_name}: ${_dest_folder}")
+
+  # Add the targets to the list of Cython extensions
+  get_property(_ext GLOBAL PROPERTY PYRODIGAL_CYTHON_EXTENSIONS)
+  list(APPEND _ext ${_target})
+  set_property(GLOBAL PROPERTY PYRODIGAL_CYTHON_EXTENSIONS ${_ext})
+endmacro()
---
+++ src/scripts/cmake/FindAVX2.cmake
@@ -0,0 +1,104 @@
+#.rst:
+# FindAVX2
+# --------
+#
+# Finds AVX2 support
+#
+# This module can be used to detect AVX2 support in a C compiler.  If
+# the compiler supports AVX2, the flags required to compile with
+# AVX2 support are returned in variables for the different languages.
+# The variables may be empty if the compiler does not need a special
+# flag to support AVX2.
+#
+# The following variables are set:
+#
+# ::
+#
+#    AVX2_C_FLAGS - flags to add to the C compiler for AVX2 support
+#    AVX2_FOUND - true if AVX2 is detected
+#
+#=============================================================================
+
+set(_AVX2_REQUIRED_VARS)
+set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
+set(CMAKE_REQUIRED_QUIET ${AVX2_FIND_QUIETLY})
+
+# sample AVX2 source code to test
+set(AVX2_C_TEST_SOURCE
+"
+#include <immintrin.h>
+void parasail_memset___m256i(__m256i *b, __m256i c, size_t len)
+{
+    size_t i;
+    for (i=0; i<len; ++i) {
+        _mm256_store_si256(&b[i], c);
+    }
+}
+int foo() {
+    __m256i vOne = _mm256_set1_epi8(1);
+    __m256i result =  _mm256_add_epi8(vOne,vOne);
+    return _mm_extract_epi16(_mm256_extracti128_si256(result,0),0);
+}
+int main(void) { return (int)foo(); }
+")
+
+# if these are set then do not try to find them again,
+# by avoiding any try_compiles for the flags
+if((DEFINED AVX2_C_FLAGS) OR (DEFINED HAVE_AVX2))
+else()
+  if(WIN32)
+    # MSVC can compile AVX intrinsics without the arch flag, however it
+    # will detect that AVX code is found and "consider using /arch:AVX".
+    set(AVX2_C_FLAG_CANDIDATES
+      "/arch:AVX"
+      "/arch:AVX2")
+  else()
+    set(AVX2_C_FLAG_CANDIDATES
+      #Empty, if compiler automatically accepts AVX2
+      " "
+      #clang
+      "-mavx2"
+      #GNU, Intel
+      "-march=core-avx2"
+    )
+  endif()
+
+  include(CheckCSourceCompiles)
+
+  foreach(FLAG IN LISTS AVX2_C_FLAG_CANDIDATES)
+    set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+    set(CMAKE_REQUIRED_FLAGS "${FLAG}")
+    unset(HAVE_AVX2 CACHE)
+    if(NOT CMAKE_REQUIRED_QUIET)
+      message(STATUS "Try AVX2 C flag = [${FLAG}]")
+    endif()
+    check_c_source_compiles("${AVX2_C_TEST_SOURCE}" HAVE_AVX2)
+    set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
+    if(HAVE_AVX2)
+      set(AVX2_C_FLAGS_INTERNAL "${FLAG}")
+      break()
+    endif()
+  endforeach()
+
+  unset(AVX2_C_FLAG_CANDIDATES)
+  
+  set(AVX2_C_FLAGS "${AVX2_C_FLAGS_INTERNAL}"
+    CACHE STRING "C compiler flags for AVX2 intrinsics")
+endif()
+
+list(APPEND _AVX2_REQUIRED_VARS AVX2_C_FLAGS)
+
+set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAVE})
+
+if(_AVX2_REQUIRED_VARS)
+  include(FindPackageHandleStandardArgs)
+
+  find_package_handle_standard_args(AVX2
+                                    REQUIRED_VARS ${_AVX2_REQUIRED_VARS})
+
+  mark_as_advanced(${_AVX2_REQUIRED_VARS})
+
+  unset(_AVX2_REQUIRED_VARS)
+else()
+  message(SEND_ERROR "FindAVX2 requires C or CXX language to be enabled")
+endif()
